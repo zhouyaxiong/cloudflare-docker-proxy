98160d8e2de2668e368d4f8468ca02546343c3c7
周亚雄 <411235298@qq.com>
2024-06-28 09:50:40 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
+import DOCS from './help.html'
+ 
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
+
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
